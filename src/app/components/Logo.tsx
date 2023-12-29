870bd615d0d04dcdf1d26dd73a2779961f0b2b56
temporis67 <ahem00@gmail.com>
2023-12-29 21:55:35 +0100

    small fixes

--- src/app/components/Logo.tsx
+++ src/app/components/Logo.tsx
@@ -1,4 +1,4 @@
-import { GlobeAltIcon, ChatBubbleLeftRightIcon  } from '@heroicons/react/24/outline';
+import { ChatBubbleLeftRightIcon  } from '@heroicons/react/24/outline';
 import { lusitana } from '@/app/lib/fonts';
 
 export default function AcmeLogo() {
@@ -7,7 +7,7 @@ export default function AcmeLogo() {
       className={`${lusitana.className} flex flex-row items-center leading-none`}
     >
       <ChatBubbleLeftRightIcon className="h-12 w-12 rotate-[345deg]" />
-      <p className="text-[48px] p-2"> PKi</p>
+      <p className="text-[48px] p-2"> Jarvis</p>
     </div>
   );
 }
--- src/app/types/definitions.ts
+++ src/app/types/definitions.ts
@@ -2,7 +2,7 @@
 // It describes the shape of the data, and what data type each property should accept.
 
 export type User = {
-    id: string;
+    uuid: string;
     name: string;
     email: string;
     password: string;
